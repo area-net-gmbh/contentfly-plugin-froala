7c0c81c5614286ef096e278bcdca70d79dd23422
areanet <apps@area-net.de>
2018-08-10 09:14:14 +0200

    Anpassung auf neue Contentfly Dev-Version

--- Froala/FroalaPlugin.php
+++ Froala/FroalaPlugin.php
@@ -9,11 +9,12 @@ class FroalaPlugin extends Plugin
     public function init(){
         $this->useFrontend();
 
-        $this->app['typeManager']->registerPluginType(new FroalaType($this->app, $this->options), $this);
+        $this->registerPluginType(new FroalaType($this->app, $this->options));
 
-        $this->app['uiManager']->addJSFile($this->getFrontendPath().'/scripts/froala_editor.pkgd.min.js');
-        $this->app['uiManager']->addCSSFile($this->getFrontendPath().'/styles/froala_editor.pkgd.min.css');
-        $this->app['uiManager']->addCSSFile($this->getFrontendPath().'/styles/froala_style.min.css');;
-        $this->app['uiManager']->addAngularModule('froala', $this->getFrontendPath().'/scripts/angular-froala.js');
+        $this->addJSFile('/scripts/froala_editor.pkgd.min.js');
+        $this->addCSSFile('/styles/froala_editor.pkgd.min.css');
+        $this->addCSSFile('/styles/froala_style.min.css');;
+        $this->addAngularModule('froala', '/scripts/angular-froala.js');
+
     }
 }
